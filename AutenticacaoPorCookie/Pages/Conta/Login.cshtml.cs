f378ad6d15c67a6d01ccc4219e94a6b8e5393910
Lucas Alcântara <lucashmalcantara@gmail.com>
2019-07-21 13:11:58 -0300

    Implementação realizada de acordo com o vídeo.

---
+++ AutenticacaoPorCookie/Pages/Conta/Login.cshtml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using AutenticacaoPorCookie.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AutenticacaoPorCookie.Pages.Conta
+{
+    public class LoginModel : PageModel
+    {
+        public async Task<IActionResult> OnPost(EntradaLoginModel entradaLoginModel)
+        {
+            if (!ModelState.IsValid)
+                return Page();
+
+
+            if (!UsuarioEstaAutenticado(entradaLoginModel.Usuario, entradaLoginModel.Senha))
+            {
+                ModelState.AddModelError(string.Empty, "Usuário ou senha inválidos.");
+                return Page();
+            }
+
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, entradaLoginModel.Usuario)
+            };
+
+            var usuarioIdentity = new ClaimsIdentity(claims, "login");
+            ClaimsPrincipal claimsPrincipal = new ClaimsPrincipal(usuarioIdentity);
+
+            await HttpContext.SignInAsync(claimsPrincipal);
+
+            return Redirect("/");
+        }
+
+        private bool UsuarioEstaAutenticado(string usuario, string senha)
+        {
+            // Simulação de autenticação no banco de dados.
+            return true;
+        }
+
+        public async Task<IActionResult> OnPostLogout()
+        {
+            await HttpContext.SignOutAsync();
+            return Redirect("/Conta/Login");
+        }
+    }
+}
---
+++ AutenticacaoPorCookie/Startup.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AutenticacaoPorCookie
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+            // Adicionar o serviço de cookies na aplicação.
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie(options => options.LoginPath = "/Conta/Login"); // Sempre que o usuário não estiver autenticado, vai cair nessa página.
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+            }
+
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+
+            // Avisa o sistema que será utilizado a autenticação.
+            app.UseAuthentication();
+
+            app.UseMvc();
+        }
+    }
+}
